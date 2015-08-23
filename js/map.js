d946bfae9ae8504683e622d5135700fd48d978ed
lgausman1 <l.gausman@comcast.net>
2015-08-23 15:29:11 -0700

    adding marker to map and rendering content in sidebar

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 // google maps code goes here
 
 function initialize() {
-	//geocoder = new google.maps.Geocoder();
+	var geocoder = new google.maps.Geocoder();
 	var mapCanvas =  document.getElementById('map');
 	var mapOptions = {
 		center: new google.maps.LatLng(37.783333, -122.416667),
@@ -11,4 +11,60 @@ function initialize() {
 
 	var map = new google.maps.Map(mapCanvas, mapOptions);
 
-}	
+	var locations = [
+		{
+			"neighborhood": "NOPA",
+			"address": "1100 Divisadero St, San Francisco, CA",
+			"phone": "415-555-1234"
+		}
+	];
+
+	console.log(locations);
+
+      // loop through posts and grab neighborhood, location and phone
+	$.each(locations,function (ind, obj) {
+		 
+		function create_marker (MapPos) {
+			var marker = new google.maps.Marker({
+				position: MapPos,
+			map: map
+			});
+
+			// set up click event and info windows to display content
+			google.maps.event.addListener(marker, 'click', (function (marker, content, infowindow) {
+				return function () {
+					$('#sidebar').empty();
+					$('#sidebar').append(content).show();
+
+				} 
+			})
+			(marker, content, infowindow)); // end listener 
+
+		} // end create_marker
+
+		//var infowindow = new google.maps.InfoWindow();
+		var infowindow = $('#sidebar').hide();
+
+// pass in the location's address 
+		var content = "<div class='well'><h3>Better Burgers</h3>" 
+							+ "<p>" + obj.neighborhood + "</p>"
+							+ "<p>" + obj.address + "</p>" 
+							+ "<p>" + obj.phone + "</p>"
+							+ "</div>";
+
+		// geocode the address and set the marker 			
+		geocoder.geocode({
+			'address': obj.address
+		}, function (results) {
+			//call marker and pass in values
+			var myPos = results[0].geometry.location;
+			create_marker(myPos);
+			
+			} // end function
+
+		); // end geocode
+
+	}); // end each
+
+
+}	// end initialize
